2ccd6b05729cff962b9ee07102f8468a3bf85e48
annzakk <annaa_z@hotmail.com>
2017-10-09 11:47:08 +0200

    with more comments

--- ParkingLot.java
+++ ParkingLot.java
@@ -17,17 +17,20 @@ public class ParkingLot {
     public ParkingLot(int numberOfSlots) {
         Random random = new Random(); //create a variable random for the for loop and for the slots
         for (int i = 0; i < numberOfSlots; i++) { //do a for loop to add a slot at the next one
-            allParkingSlots.add(new Slot(1 + random.nextInt(3))); //add at the next
+            allParkingSlots.add(new Slot(1 + random.nextInt(5))); //add at the next
         }
         // all slots initially free
         freeParkingSlots.addAll(allParkingSlots);
     }
 
-   public Ticket parkAutomobile(Automobile Automobile) {
+   public Ticket parkAutomobile(Automobile Automobile) { //this method is going to be used in parkinglotmanager
         Slot targetSlot = freeParkingSlots.stream().filter(p -> p.accepts(Automobile)).findFirst()
-                .orElseThrow(() -> new RuntimeException("No free slot for " + Automobile));
-        // use stream to read the different , 
-        targetSlot.addAutomobile(Automobile);
+                .orElseThrow(() -> new RuntimeException("No free slot for " + Automobile)); 
+        //stream is to itterate over the whole collection of slot. and in the nezt method is what you actually want to do when u itterate over it
+        //filter those elements that are free, so TAKE the first free one
+        //need the p(current object) for lamda expresson to dont get an error, and go to the accpt method
+        //accept gives u a boolean, IF false we throw an exception. and a runtime exception makes it stop
+        targetSlot.addAutomobile(Automobile); //add vehicle in slot 
         if (!targetSlot.isFree()) { //if its not free remove it
             freeParkingSlots.remove(targetSlot);
         }
@@ -45,7 +48,7 @@ public class ParkingLot {
     @Override
     public String toString() {
         return String.format("ParkingLot [income=%.2f, freeParkingSlots=%d, parkingAutomobiles=%d]", income, freeParkingSlots.size(),
-                parkingAutomobiles.size());
+                parkingAutomobiles.size()); //format flow with 2 decimals, The %d specifies that the single variable is a decimal integer
     }
     
 }
